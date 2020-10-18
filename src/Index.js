c03518e0a1025a01caa2a7bdd38eb173e2b45441
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2020-10-18 19:00:24 -0400

    refactoring various

--- src/Index.js
+++ src/Index.js
@@ -1,20 +1,20 @@
 require('app-module-path').addPath("../CognitiveFunc");
 
-const Discord = require('discord.js');
-const Client = new Discord.Client();
-var auth = require('auth.json');
-var lib = require('lib/lib.js');
+const discord = require('discord.js');
+const auth = require('auth.json');
+const lib = require('lib/lib.js');
 
-Client.on('ready', () => {
-   console.log(`Logged in as ${Client.user.tag}!`);
+const client = new discord.Client();
+
+client.on('ready', () => {
+   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-Client.on('message', msg => {
-  console.log(msg.content);
+client.on('message', msg => {
   if (msg.content.substring(0, 1) == '!') {
        var str = lib.Selector(msg.content);
        msg.reply(str);
      }
   });
 
-Client.login(auth.Token);
+client.login(auth.Token);
