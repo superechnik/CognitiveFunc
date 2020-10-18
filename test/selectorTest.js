3eabf9e14d9c80fbd8ce4d6ddb4377a9c25e8c48
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2020-10-18 16:06:25 -0400

    refactor code to lib

---
+++ test/selectorTest.js
@@ -0,0 +1,14 @@
+require('app-module-path').addPath('../CognitiveFunc');
+var lib = require('Lib/lib.js');
+
+const assert = require('assert');describe('Responds to !Fi', () => {
+ it('should not be null', () => {
+        assert(lib.Selector('!Fi') != null && lib.Selector('!Fi') != 'dunno');
+    });
+});
+
+const assert2 = require('assert');describe('Responds to !xxx', () => {
+ it('should be dunno', () => {
+        assert2(lib.Selector('!Foo') == 'dunno');
+    });
+});
